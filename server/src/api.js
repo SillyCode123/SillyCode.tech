ff1fd6e8056afc19a290aeedabe14c857fdc5cbd
SillyCodeDev <bartoc.junior@gmail.com>
2023-03-26 18:51:42 +0200

    pleas mthf

--- server/src/api.js
+++ server/src/api.js
@@ -1,8 +1,9 @@
+'use strict'
 //import
 const express = require('express');
 const serverless = require('serverless-http');
 require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require("mysql");
 
 //Express
 const app = express();
@@ -12,9 +13,11 @@ const router = express.Router();
 //other vars
 const indexHtml = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>SillyCodeAPI</title></head><body><a href="https://sillycode.tech" style="color: #0086f5" class=".zoom">Click here to go to our Main Page</a> <style> body{--color-darkgrey: #1f1e1e;--color-white: #ffffff;--font: "Red_Hat_Mono", monospace;font-family: "Red_Hat_Mono", monospace;font-weight: 400;font-style: normal;font-size: calc(1rem + 0.4vw);line-height: 3ch;color: var(--color-white);background-color: var(--color-darkgrey);text-align: center;} a {text-decoration: none;}</style></body></html>';
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user:process.env.DB_USER,
-  password: process.env.DB_PSW
+  host    : process.env.DB_HOST,
+  user    : process.env.DB_USER,
+  port    : '3306',
+  password: process.env.DB_PSW,
+  database: "mariadbdb",
 });
 
 
@@ -35,11 +38,11 @@ app.use((err, req, res, next) => {
 //Routes
 router.get("/test", (req,res) => {
   try {
-    res.write(executeQuery("Select * from SillyCodeTech.DiscordLoginCookies;") + "")
+    res.write(executeQuery("Select * from SillyCodeTech.DiscordLoginCookies;") + "");
   } catch (error) {
     res.write(error);
+    console.error(error);
   }
- 
   res.end();
 });
 
@@ -47,8 +50,9 @@ function executeQuery(cmd) {
   connection.query(
     cmd,
     function(err, results, fields) {
-      console.log(results); // results contains rows returned by server
-      console.log(fields); // fields contains extra meta data about results, if available
+      console.log(err);
+      console.log("RES:" + results); // results contains rows returned by server
+      console.log("F:" + fields); // fields contains extra meta data about results, if available
       return results;
     }
   );
