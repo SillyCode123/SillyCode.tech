91bd04325414a7dd05b71fbbcd38660ae2bcdf7d
SillyCodeDev <bartoc.junior@gmail.com>
2023-04-21 21:23:37 +0200

    forgot to include menubar in index index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,11 @@
 import "../css/App.css";
-import logo from "../img/SillyCoder.png";
+import MenuBar from "./componets/MenuBar";
 import { motion } from "framer-motion";
 
 function Index() {
-  const dash = () => {
-    window.location = "/dashboard";
-  };
-    
-  const mem = () => {
-    window.location = "/devteam";
-  };
-
   return (
     <>
-      <div className="navbar">
-        <div className="logoNav">
-          <motion.a
-            href="index"
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            className="logoBtn"
-          >
-            <img
-              className="logo"
-              alt=""
-              src={logo}
-              width="150px"
-              height="150px"
-            ></img>
-          </motion.a>
-
-          <motion.button
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            className="DashboardBtn"
-            onClick={dash}
-          >
-            Dashboard
-          </motion.button>
-          <motion.button
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.9 }}
-            className="aboutusbtn"
-            onClick={mem}
-          >
-            About Us
-          </motion.button>
-        </div>
-      </div>
+      <MenuBar/>  
       <br/>
       <main style={{margin: "0", padding: "0"}}>
         <section className="bgblue">
